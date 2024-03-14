6de1db69e210cfe189f65fb94643b3d9535a3868
FELLIPE MARTINS VALLADARES <202401000991@alunos.ibmec.edu.br>
2024-03-14 11:32:17 -0300

    sockets

---
+++ aula3/tcp/cliente_tcp.py
@@ -0,0 +1,18 @@
+from socket import socket, AF_INET, SOCK_STREAM
+
+server_name = 'localhost'
+server_port = 12005
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((server_name, server_port))
+
+msg = input('Entre minúsculas \n')
+
+client_socket.send(msg.encode())
+
+msg_modificada = client_socket.recv(1024)
+
+print(msg_modificada.decode())
+print(client_socket)
+
+client_socket.close()
